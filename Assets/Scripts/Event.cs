29d60e37a09df53e42b3419391bd93b438a0a724
Menshikov Vlad <mevlme44@inbox.ru>
2021-05-08 10:27:01 +0300

    Add ui logic and some api

--- Assets/Scripts/Event.cs
+++ Assets/Scripts/Event.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Event : MonoBehaviour
+namespace Interface 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public class Event : MonoBehaviour {
+        public string Name;
+        public string Date;
+        public Vector2 Coordinates;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public Text eventName;
+
+        void Start() {
+
+        }
+
+        public void Instantiate(string name, string date, Vector2 coords) {
+            var newButton = Instantiate(gameObject, transform.parent);
+            newButton.SetActive(true);
+
+            var newEvent = newButton.GetComponent<Event>();
+            newEvent.SetParams(name, date, coords);
+
+            if(transform.parent.childCount <= 2) return;
+
+            var rect = transform.parent.gameObject.GetComponent<RectTransform>();
+            rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, rect.rect.height + 185f);
+        }
+
+        public void SetParams(string name, string date, Vector2 coords) {
+            eventName.text = name;
+            Name = name;
+            Date = date;
+            Coordinates = coords;
+            PlayerPrefs.Save();
+        }
     }
 }
---
+++ Assets/Scripts/Save.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Save : MonoBehaviour
+{
+    public Text Name;
+    public Text Date;
+    public Interface.Event ButtonTemplate;
+
+    void Awake() {
+
+    }
+
+    public void OnConfrim() {
+        ButtonTemplate.Instantiate(Name.text, Date.text, Vector2.one);
+        //PlayerPrefs.SetString(Name.text, Date.text);
+    }
+}
